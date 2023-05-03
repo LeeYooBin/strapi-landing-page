d70969eeac92b678e6015f2734578027c82d8c57
LeeYooBin <santosroberdan25@gmail.com>
2023-05-03 19:25:00 -0300

    Adding Zion Lunar Resort landing page configs

---
+++ src/api/another-page/controllers/another-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * another-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::another-page.another-page');
---
+++ src/api/another-page/routes/another-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * another-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::another-page.another-page');
